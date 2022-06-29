432898bca005569804d5fee6696b69074eaf561e
harish5058 <hariharsangale@gmail.com>
2022-06-29 14:35:53 +0530

    Pattern3.java

--- src/Patterns/Pattern3.java
+++ src/Patterns/Pattern3.java
@@ -1,4 +1,5 @@
 package Patterns;
+import java.util.Date;
 import java.util.Scanner;
     /*      *
           * *
@@ -10,6 +11,8 @@ public class Pattern3 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number");
         int n = sc.nextInt();
+        long startTime = System.currentTimeMillis();
+        //o(n2)
         for (int i = 0; i < n; i++) {
             for (int j = 0; j<n; j++) {
                 if(j>=n-1-i)
@@ -19,5 +22,27 @@ public class Pattern3 {
             }
             System.out.println();
         }
+        long endTime = System.currentTimeMillis();
+        System.out.println("endTime "+endTime);
+        System.out.println("total time taken "+((endTime-startTime)/1000));
+
+        System.out.println("++++++++++++++++++");
+        // o(n3)
+       /* long startTime = System.currentTimeMillis();
+        System.out.println("startTime "+startTime);
+        Date d1 = new Date();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-1-i;j++){
+                System.out.print(" "+" ");
+            }
+            for(int k=0;k<=i;k++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+        Date d2 = new Date();
+        long endTime = System.currentTimeMillis();
+        System.out.println("endTime "+endTime);
+        System.out.println("total time taken "+((endTime-startTime)/1000));*/
     }
 }

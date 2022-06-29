eb4a1e36ee5223801c98e8922d3434dd2da1a870
harish5058 <hariharsangale@gmail.com>
2022-06-29 07:21:01 +0530

    TwoSum commit

---
+++ src/LeetCode/TwoSum.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+import java.util.Scanner;
+
+public class TwoSum {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter array size");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter " + size + "numbers");
+        for (int i = 0; i <size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter k");
+        int k = sc.nextInt();
+        for(int i=0;i<size-1;i++){
+            for(int j=i+1;j<size;j++){
+                if(arr[i]+arr[j]==k){
+                    System.out.println("{"+arr[i]+","+arr[j]+"}");
+                }
+            }
+        }
+    }
+}

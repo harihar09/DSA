6abe34cd5e741a751a42629f9dad3341bf6173e8
harish5058 <hariharsangale@gmail.com>
2022-07-13 16:07:30 +0530

    new class files

---
+++ src/Java7/Serialization/Dog.java
@@ -0,0 +1,7 @@
+package Java7.Serialization;
+
+import java.io.Serializable;
+
+public class Dog implements Serializable {
+    Cat c = new Cat();
+}
---
+++ src/Java7/Serialization/Rat.java
@@ -0,0 +1,7 @@
+package Java7.Serialization;
+
+import java.io.Serializable;
+
+public class Rat implements Serializable {
+    int j=20;
+}

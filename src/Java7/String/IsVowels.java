4822440f8bd8072ca88253b80de1e19449208c4f
harish5058 <hariharsangale@gmail.com>
2022-09-06 00:32:08 +0530

    new files

---
+++ src/Java7/String/IsVowels.java
@@ -0,0 +1,11 @@
+package Java7.String;
+
+public class IsVowels {
+    public static boolean isVowel(char ch){
+        String vowels = "aeiouAEIOU";
+        return vowels.indexOf(ch)!=-1?true:false;
+    }
+    public static void main(String[] args) {
+
+    }
+}

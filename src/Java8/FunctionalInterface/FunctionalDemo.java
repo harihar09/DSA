d9f8c435f1923b153c6734771a03336286c90841
harish5058 <hariharsangale@gmail.com>
2023-06-03 18:43:21 +0530

    removed files

--- src/Java8/FunctionalInterface/FunctionalDemo.java
+++
@@ -1,7 +0,0 @@
-package Java8.FunctionalInterface;
-
-@FunctionalInterface
-public interface FunctionalDemo {
-    public int add(int a,int b);
-}
-
--- src/Java8/FunctionalInterface/FunctionalDemoImplUsing8.java
+++
@@ -1,9 +0,0 @@
-package Java8.FunctionalInterface;
-
-public class FunctionalDemoImplUsing8 {
-    public static void main(String[] args) {
-        FunctionalDemo functionalDemo = (a,b)->a+b;
-        int ans = functionalDemo.add(20,30);
-        System.out.println(ans);
-    }
-}

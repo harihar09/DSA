d71343c48af118834034cbe5ac5bbcfd11a283e3
harish5058 <hariharsangale@gmail.com>
2024-01-11 10:55:01 +0530

    PlainLL

--- src/Algo/LinkedList/SingleLL/PlainLL.java
+++ src/Algo/LinkedList/SingleLL/PlainLL.java
@@ -17,23 +17,34 @@ public class PlainLL {
     }
 
 
-    private static void printLL(Node firstNode){
+    private static void printLL(Node firstNode) {
         Node temp = firstNode;
-        while(temp!=null){
-            System.out.print(temp.data+" ");
+        while (temp != null) {
+            System.out.print(temp.data + " ");
             temp = temp.next;
         }
 
     }
+
     public static void main(String[] args) {
+        // initialization with first constructor
         Node firstNode = new Node();
         Node secondNode = new Node();
         Node thirdNode = new Node();
-
         firstNode.next = secondNode;
         secondNode.next = thirdNode;
-
         printLL(firstNode);
+        System.out.println();
+        // initialization with parameterized constructor
+        Node first = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+        Node fourth = new Node(40);
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        printLL(first);
+
 
     }
 }

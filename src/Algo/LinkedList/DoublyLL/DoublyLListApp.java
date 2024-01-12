cd825a265fb2ec2d730f4c700e6496a70a2e73c3
harish5058 <hariharsangale@gmail.com>
2024-01-12 22:20:15 +0530

    Doubly ll

--- src/Algo/LinkedList/DoublyLL/DoublyLListApp.java
+++ src/Algo/LinkedList/DoublyLL/DoublyLListApp.java
@@ -73,6 +73,77 @@ public class DoublyLListApp {
         node.next = currNode;
     }
 
+    //delete at head
+    private void deleteAtHead() {
+        //empty check
+        if (head == null) {
+            System.out.println("list is empty..");
+            return;
+        }
+        // single node
+        if (head.next == null) {
+            head = null;
+            tail = null;
+            return;
+        }
+        //normal flow - update the head and adjust the pointers
+        head = head.next;
+        head.prev.next = null;
+        head.prev = null;
+    }
+
+    //delete at tail
+    private void deleteAtTail() {
+        //empty check
+        if (head == null) {
+            System.out.println("list is empty..");
+            return;
+        }
+        //corner case - single node
+        if (head.next == null) {
+            head = null;
+            tail = null;
+            return;
+        }
+        //normal flow - update the tail and pointers
+        tail = tail.prev;
+        tail.next.prev = null;
+        tail.next = null;
+    }
+
+    //delete at given index
+    private void deleteAtIndex(int index) {
+        //empty check
+        if (head.next == null) {
+            System.out.println("list is empty..");
+            return;
+        }
+        if (index <= 0 || index > listLength()) {
+            System.out.println("invalid index");
+            return;
+        }
+        //corner case : first and last index
+        if (index == 1) {
+            deleteAtHead();
+        } else if (index == listLength()) {
+            deleteAtTail();
+        } else {
+            // find out pre,curr node and repoint the pointers
+            int temp = 1;
+            index = index - 1;
+            DoublyLListNode prevNode = head;
+            while (temp < index) {
+                prevNode = prevNode.next;
+                temp++;
+            }
+            DoublyLListNode currNode = prevNode.next;
+            prevNode.next = currNode.next;
+            currNode.prev = null;
+            currNode.next.prev = prevNode;
+            currNode.next = null;
+        }
+    }
+
     // get list length
     private int listLength() {
         DoublyLListNode temp = head;
@@ -83,6 +154,7 @@ public class DoublyLListApp {
         }
         return len;
     }
+
 
     //print list data
     private void print() {
@@ -109,6 +181,20 @@ public class DoublyLListApp {
         list.print();
         System.out.println();
         list.insertAtPosition(100, 2);
+        list.print();
+        System.out.println();
+      /*  list.deleteAtHead();
+        list.deleteAtHead();
+        list.print();
+        System.out.println();
+        list.deleteAtTail();
+        list.deleteAtTail();
+        list.print();
+        System.out.println();
+        list.deleteAtTail();
+        list.print();
+        System.out.println();*/
+        list.deleteAtIndex(4);
         list.print();
     }
 }

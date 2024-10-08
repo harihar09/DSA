06940d1c760c46ec51088b0fdb6dba33b486e3a0
ROHINIMUSALE <rohinimusale9@gmail.com>
2024-10-08 15:12:36 +0530

    adding loggers

--- src/Algo/LinkedList/SingleLL/ReverseListApp.java
+++ src/Algo/LinkedList/SingleLL/ReverseListApp.java
@@ -1,7 +1,12 @@
 package Algo.LinkedList.SingleLL;
 
+
+import java.util.logging.Logger;
+
 public class ReverseListApp {
+    static Logger logger = Logger.getLogger(String.valueOf(ReverseListApp.class));
     public static void main(String[] args) {
+        logger.info("inside main method.");
         ReverseList reverseList = new ReverseList();
         reverseList.add(10);
         reverseList.traverse();
